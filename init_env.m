dcd1cb89ba929ef362cd46b510aa5576ab4fa49e
ppadmanabhan6 <priyanka.91092@gmail.com>
2015-04-29 16:50:30 -0400

    First Commit

--- init_env.m
+++
@@ -1,4 +0,0 @@
-% check BNT toolbox, this is used by some agents
-if isempty(which('mk_bnet'))
-    warning('BNT not found. Add BNT folder to path\n');
-end
--- poker_demo.m
+++
@@ -1,6 +0,0 @@
-clear;
-close all;
-
-init_env;
-
-poker_main('agents_bin');
